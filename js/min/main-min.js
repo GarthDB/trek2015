d388c16f518aaedba55d7d6d82b0b24f599f9ad7
garthdb <garthdb@gmail.com>
2015-01-04 00:10:15 -0700

    have the basic website working

---
+++ js/min/main-min.js
@@ -0,0 +1,21 @@
+function smoothScroll(el, to, duration) {
+  if (duration < 0) {
+    return;
+  }
+  var difference = to - $(window).scrollTop();
+  var perTick = difference / duration * 10;
+  this.scrollToTimerCache = setTimeout(function() {
+    if (!isNaN(parseInt(perTick, 10))) {
+      window.scrollTo(0, $(window).scrollTop() + perTick);
+      smoothScroll(el, to, duration - 10);
+    }
+  }.bind(this), 10);
+}
+Zepto(function($){
+  $('nav.main a').on('click', function(e) {
+    e.preventDefault();
+    smoothScroll($(window), $($(e.currentTarget).attr('href')).offset().top, 200);
+  });
+});
+
+
